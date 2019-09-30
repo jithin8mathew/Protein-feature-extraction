b0a5b2d508a34ac213e248b58bf8f98076a4e7cd
jithin8mathew <jithinjosemathewoo8@gmail.com>
2019-09-30 18:36:52 -0500

    Add existing file

--- ML.py
+++ ML.py
@@ -1,12 +1,21 @@
 import subprocess
+import sys
+
+system = sys.platform
 
 def fasta_process():
 	print("Processing fasta files....")
-	subprocess.run(['python.exe','process_fasta.py'])
+	if system == 'linux':
+		subprocess.run(['python3','process_fasta.py'])
+	else:
+		subprocess.run(['python.exe','process_fasta.py'])
 
 def feat_ext():
 	print("Extracting features....")
-	subprocess.run(['python.exe','feature_extraction.py'])
+	if system == 'linux':
+		subprocess.run(['python3','feature_extraction.py'])
+	else:
+		subprocess.run(['python.exe','process_fasta.py'])
 
 if __name__ == '__main__':
 	fasta_process()
@@ -15,5 +24,3 @@ if __name__ == '__main__':
 	print("Extracted features are saved in data/output directory in .txt, .arff and .csv formats")
 	import os
 	print("Have a great day ",os.getlogin(), "!")
-	
-
--- process_fasta.py
+++ process_fasta.py
@@ -2,18 +2,18 @@ from Bio import SeqIO
 import os
 
 path_=os.getcwd()  			# get the location of Current working directory
-_=(os.path.join(path_,"data\\data.txt"))
+_=(os.path.join(path_,"data/data.txt"))
 
 if os.path.isfile(_):
-	try:os.remove("data\\data.txt")		# Remove the data.txt file from previous run
+	try:os.remove("data/data.txt")		# Remove the data.txt file from previous run
 	except Exception:pass
 
-o_put=open("data\\data.txt","a+")		# create and open a new data.txt file
+o_put=open("data/data.txt","a+")		# create and open a new data.txt file
 o_put.write("sequence"+"\t"+"class"+"\n")
-for record in SeqIO.parse("data\\positive_training.fasta", "fasta"):		# Code to parse multiple sequences using Bio Python
+for record in SeqIO.parse("data/positive_training.fasta", "fasta"):		# Code to parse multiple sequences using Bio Python
 	o_put.write(str(record.seq)+"\t"+"1"+"\n")								# Positive sequence output
-	
-for record in SeqIO.parse("data\\negative_training.fasta", "fasta"):		# Code to parse multiple sequences using Bio Python
+
+for record in SeqIO.parse("data/negative_training.fasta", "fasta"):		# Code to parse multiple sequences using Bio Python
 	o_put.write(str(record.seq)+"\t"+"0"+"\n")								# Negative sequence output
 
 o_put.close()
